fbbc6f642fd07b588c4e6ce967ef4054cb6c1887
Sahil Limbe <sahil.d.limbe@gmail.com>
2020-10-23 17:16:52 +0530

    added lesson details and html parser for body of lesson

--- lib/screens/lesson_screen.dart
+++ lib/screens/lesson_screen.dart
@@ -1,7 +1,9 @@
 import 'package:correcting_time/models/lesson_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 class LessonScreen extends StatelessWidget {
+  final htmlData = """<h1>Hello</h1>""";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +16,44 @@ class LessonScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text(
-                "Teachers: " + periodOfRecess.teachers,
-                style: TextStyle(
-                  // color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  " Teachers: " + periodOfRecess.teachers,
+                  style: TextStyle(
+                    // color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              // Text(periodOfRecess.body),
-            ],
+                SizedBox(
+                  height: 16,
+                ),
+                // Text(periodOfRecess.body),
+                Html(
+                  data: periodOfRecess.body,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Image.network(
+                  periodOfRecess.image,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Location: " + periodOfRecess.location,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 17,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
